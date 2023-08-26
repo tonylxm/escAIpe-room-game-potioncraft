f43fe49bfcdbb2bb813f23d91894b4f884d44af5
Gluglug7 <abod278@aucklanduni.ac.nz>
2023-08-27 09:13:44 +1200

    ✨ Go right button added and implemented in ShelfLeft scene

---
+++ src/main/java/nz/ac/auckland/se206/controllers/ShelfLeftController.java
@@ -0,0 +1,17 @@
+package nz.ac.auckland.se206.controllers;
+
+import javafx.fxml.FXML;
+import javafx.scene.shape.Polygon;
+import javafx.scene.shape.Rectangle;
+
+public class ShelfLeftController {
+    @FXML private Rectangle itemOneRect;
+    @FXML private Rectangle itemTwoRect;
+    @FXML private Rectangle itemThreeRect;
+    @FXML private Rectangle itemFourRect;
+    @FXML private Rectangle itemFiveRect;
+    @FXML private Polygon rightShpe;
+
+    @FXML public void goT=Right(){
+    }
+}
