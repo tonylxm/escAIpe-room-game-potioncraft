94c857badefd2eb85d3a3c6d27f08db32781c295
Andy <azha405@aucklanduni.ac.nz>
2023-08-27 13:09:47 +1200

    ? Implmeneted left and right arrows to change scenes

--- src/main/java/nz/ac/auckland/se206/controllers/CauldronRoomController.java
+++ src/main/java/nz/ac/auckland/se206/controllers/CauldronRoomController.java
@@ -1,12 +1,18 @@
 package nz.ac.auckland.se206.controllers;
 
 import javafx.fxml.FXML;
+import javafx.scene.Scene;
 import javafx.scene.input.MouseEvent;
+import javafx.scene.shape.Polygon;
 import javafx.scene.shape.Rectangle;
+import nz.ac.auckland.se206.SceneManager;
+import nz.ac.auckland.se206.SceneManager.AppUi;
 
 public class CauldronRoomController {
   @FXML private Rectangle cauldronRectangle;
   @FXML private Rectangle wizardRectangle;
+  @FXML private Polygon rightArrow;
+  @FXML private Polygon leftArrow;
 
   @FXML
   public void clickCauldron(MouseEvent event) {
@@ -16,5 +22,19 @@ public class CauldronRoomController {
   @FXML
   public void clickWizard(MouseEvent event) {
     System.out.println("wizard clicked");
+  }
+
+  @FXML
+  public void goLeft(MouseEvent event) {
+    System.out.println("CAULDRON ROOM > SHELF LEFT");
+    Scene currentScene = cauldronRectangle.getScene();
+    currentScene.setRoot(SceneManager.getUiRoot(AppUi.SHELF_LEFT));
+  }
+
+  @FXML
+  public void goRight(MouseEvent event) {
+    System.out.println("CAULDRON ROOM > SHELF RIGHT");
+    Scene currentScene = cauldronRectangle.getScene();
+    currentScene.setRoot(SceneManager.getUiRoot(AppUi.SHELF_RIGHT));
   }
 }
