9f4d0e619a7ed21c3e0b5772993628a36ea5fbf8
Gluglug7 <abod278@aucklanduni.ac.nz>
2023-08-28 14:42:24 +1200

    Using ShapeInteractionHandler for glowing in shelf_left and refactoring text code to setText method

--- src/main/java/nz/ac/auckland/se206/controllers/ShelfLeftController.java
+++ src/main/java/nz/ac/auckland/se206/controllers/ShelfLeftController.java
@@ -4,7 +4,6 @@ import java.util.Iterator;
 
 import javafx.fxml.FXML;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.shape.Polygon;
@@ -12,12 +11,12 @@ import javafx.scene.shape.Rectangle;
 import nz.ac.auckland.se206.Items;
 import nz.ac.auckland.se206.SceneManager;
 import nz.ac.auckland.se206.SceneManager.AppUi;
+import nz.ac.auckland.se206.ShapeInteractionHandler;
 
 public class ShelfLeftController {
     public boolean itemOnePicked, itemTwoPicked, itemThreePicked, itemFourPicked, itemFivePicked;
     public boolean readyToAdd;
     public Items.Item item;
-
 
     @FXML private Rectangle itemOneRect;
     @FXML private Rectangle itemTwoRect;
@@ -31,6 +30,8 @@ public class ShelfLeftController {
     @FXML private Label yesLbl;
     @FXML private Label dashLbl;
 
+    @FXML private ShapeInteractionHandler interactionHandler;
+
     public void initialize() {
         itemOnePicked = false;
         itemTwoPicked = false;
@@ -38,6 +39,32 @@ public class ShelfLeftController {
         itemFourPicked = false;
         itemFivePicked = false;
         readyToAdd = false;
+
+        interactionHandler = new ShapeInteractionHandler();
+        if (itemOneRect != null) {
+            itemOneRect.setOnMouseEntered(event -> interactionHandler.handle(event));
+            itemOneRect.setOnMouseExited(event -> interactionHandler.handle(event));
+        }
+        if (itemTwoRect != null) {
+            itemTwoRect.setOnMouseEntered(event -> interactionHandler.handle(event));
+            itemTwoRect.setOnMouseExited(event -> interactionHandler.handle(event));
+        }
+        if (itemThreeRect != null) {
+            itemThreeRect.setOnMouseEntered(event -> interactionHandler.handle(event));
+            itemThreeRect.setOnMouseExited(event -> interactionHandler.handle(event));
+        }
+        if (itemFourRect != null) {
+            itemFourRect.setOnMouseEntered(event -> interactionHandler.handle(event));
+            itemFourRect.setOnMouseExited(event -> interactionHandler.handle(event));
+        }
+        if (itemFiveRect != null) {
+            itemFiveRect.setOnMouseEntered(event -> interactionHandler.handle(event));
+            itemFiveRect.setOnMouseExited(event -> interactionHandler.handle(event));
+        }
+        if (rightShpe != null) {
+            rightShpe.setOnMouseEntered(event -> interactionHandler.handle(event));
+            rightShpe.setOnMouseExited(event -> interactionHandler.handle(event));
+        }
     }
 
     /** Changing scenes to the cauldron room */
@@ -47,150 +74,54 @@ public class ShelfLeftController {
         Scene currentScene = rightShpe.getScene();
         currentScene.setRoot(SceneManager.getUiRoot(AppUi.CAULDRON_ROOM));
     }
-
-    /** Highlighting when hovered over */
-    @FXML
-    public void goRightGlow() {
-        rightShpe.setStrokeWidth(5);
-    }
-
-    @FXML
-    public void goRightGlowExit() {
-        rightShpe.setStrokeWidth(0);
-    }
-
-    /** Highlighting when hovered over */
-    @FXML
-    public void itemOneGlow() {
-        if (itemOnePicked) return;
-        itemOneRect.setStrokeWidth(5);
-    }
-
-    @FXML void itemOneGlowExit() {
-        if (itemOnePicked) return;
-        itemOneRect.setStrokeWidth(0);
-    }
     
     /** Selecting item one and prompting user to add to inventory */
     @FXML
     public void itemOneSelect() {
         if (itemOnePicked) return;
-        textLbl.setText("Add to inventory?");
-        textRect.setOpacity(1);
-        textLbl.setOpacity(1);
-        yesLbl.setOpacity(1);
-        noLbl.setOpacity(1);
-        dashLbl.setOpacity(1);
+        setText("Add to inventory?", true);
         item = Items.Item.ITEM_1;
         readyToAdd = true;
-    }
-
-    /** Highlighting when hovered over */
-    @FXML
-    public void itemTwoGlow() {
-        if (itemTwoPicked) return;
-        itemTwoRect.setStrokeWidth(5);
-    }
-
-    @FXML
-    public void itemTwoGlowExit() {
-        if (itemTwoPicked) return;
-        itemTwoRect.setStrokeWidth(0);
     }
 
     /** Selecting item two and prompting user to add to inventory */
     @FXML
     public void itemTwoSelect() {
         if (itemTwoPicked) return;
-        textLbl.setText("Add to inventory?");
-        textRect.setOpacity(1);
-        textLbl.setOpacity(1);
-        yesLbl.setOpacity(1);
-        noLbl.setOpacity(1);
-        dashLbl.setOpacity(1);
+        setText("Add to inventory?", true);
         item = Items.Item.ITEM_2;
         readyToAdd = true;
-    }
-
-    /** Highlighting when hovered over */
-    @FXML
-    public void itemThreeGlow() {
-        if (itemThreePicked) return;
-        itemThreeRect.setStrokeWidth(5);
-    }
-
-    @FXML
-    public void itemThreeGlowExit() {
-        if (itemThreePicked) return;
-        itemThreeRect.setStrokeWidth(0);
+        System.out.println("item two clicked");
     }
 
     /** Selecting item three and prompting user to add to inventory */
     @FXML
     public void itemThreeSelect() {
         if (itemThreePicked) return;
-        textLbl.setText("Add to inventory?");
-        textRect.setOpacity(1);
-        textLbl.setOpacity(1);
-        yesLbl.setOpacity(1);
-        noLbl.setOpacity(1);
-        dashLbl.setOpacity(1);
+        setText("Add to inventory?", true);
         item = Items.Item.ITEM_3;
         readyToAdd = true;
-    }
-
-    /** Highlighting when hovered over */
-    @FXML
-    public void itemFourGlow() {
-        if (itemFourPicked) return;
-        itemFourRect.setStrokeWidth(5);
-    }
-
-    @FXML
-    public void itemFourGlowExit() {
-        if (itemFourPicked) return;
-        itemFourRect.setStrokeWidth(0);
+        System.out.println("item three clicked");
     }
 
     /** Selecting item four and prompting user to add to inventory */
     @FXML
     public void itemFourSelect() {
         if (itemFourPicked) return;
-        textLbl.setText("Add to inventory?");
-        textRect.setOpacity(1);
-        textLbl.setOpacity(1);
-        yesLbl.setOpacity(1);
-        noLbl.setOpacity(1);
-        dashLbl.setOpacity(1);
+        setText("Add to inventory?", true);
         item = Items.Item.ITEM_4;
         readyToAdd = true;
-    }
-
-    /** Highlighting when hovered over */
-    @FXML
-    public void itemFiveGlow() {
-        if (itemFivePicked) return;
-        itemFiveRect.setStrokeWidth(5);
-    }
-
-    @FXML
-    public void itemFiveGlowExit() {
-        if (itemFivePicked) return;
-        itemFiveRect.setStrokeWidth(0);
+        System.out.println("item four clicked");
     }
 
     /** Selecting item five and prompting user to add to inventory */
     @FXML
     public void itemFiveSelect() {
         if (itemFivePicked) return;
-        textLbl.setText("Add to inventory?");
-        textRect.setOpacity(1);
-        textLbl.setOpacity(1);
-        yesLbl.setOpacity(1);
-        noLbl.setOpacity(1);
-        dashLbl.setOpacity(1);
+        setText("Add to inventory?", true);
         item = Items.Item.ITEM_5;
         readyToAdd = true;
+        System.out.println("item five clicked");
     }
 
     /** Adding item to inventory if an item is selected */
@@ -198,12 +129,7 @@ public class ShelfLeftController {
     public void addItem() {
         if (!readyToAdd) return;
         MainMenuController.inventory.add(item);
-        textLbl.setText("");
-        textRect.setOpacity(0);
-        textLbl.setOpacity(0);
-        yesLbl.setOpacity(0);
-        noLbl.setOpacity(0);
-        dashLbl.setOpacity(0);
+        setText("", false);
         readyToAdd = false;
 
         switch(item) {
@@ -228,8 +154,8 @@ public class ShelfLeftController {
                 itemFivePicked = true;
                 break;
         }
-
-        System.out.println();
+        System.out.println("Item added to inventory");
+        System.out.println("Current Inventory:");
         Iterator itr = new MainMenuController().inventory.inventory.iterator();
         while (itr.hasNext()) {
             System.out.println(itr.next());
@@ -240,12 +166,26 @@ public class ShelfLeftController {
     @FXML
     public void noAdd() {
         if (!readyToAdd) return;
-        textLbl.setText("");
-        textRect.setOpacity(0);
-        textLbl.setOpacity(0);
-        yesLbl.setOpacity(0);
-        noLbl.setOpacity(0);
-        dashLbl.setOpacity(0);
+        setText("", false);
         readyToAdd = false;
+        System.out.println("Item not added to inventory");
+    }
+
+    @FXML
+    private void setText(String text, boolean on) {
+        textLbl.setText(text);
+        if (on) {
+            textRect.setOpacity(1);
+            textLbl.setOpacity(1);
+            yesLbl.setOpacity(1);
+            noLbl.setOpacity(1);
+            dashLbl.setOpacity(1);
+        } else {
+            textRect.setOpacity(0);
+            textLbl.setOpacity(0);
+            yesLbl.setOpacity(0);
+            noLbl.setOpacity(0);
+            dashLbl.setOpacity(0);
+        }
     }
 }
