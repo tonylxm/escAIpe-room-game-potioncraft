10e87d72d55ede53f52ab22702dbf5ffa937629a
Gluglug7 <abod278@aucklanduni.ac.nz>
2023-09-01 10:26:30 +1200

    Changing isRiddleResolved to appropriate book riddle variable

--- src/main/java/nz/ac/auckland/se206/controllers/BookController.java
+++ src/main/java/nz/ac/auckland/se206/controllers/BookController.java
@@ -101,7 +101,7 @@ public class BookController {
     appendChatMessage(msg);
     ChatMessage lastMsg = runGpt(msg);
     if (lastMsg.getRole().equals("assistant") && lastMsg.getContent().startsWith("Correct")) {
-      GameState.isRiddleResolved = true;
+      GameState.isBookRiddleResolved = true;
     }
   }
 
