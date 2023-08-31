205e690d0b5dada838f434bcf33ac9b29bffdeab
Andy <azha405@aucklanduni.ac.nz>
2023-09-01 10:33:04 +1200

    ✨Added Timer class

--- src/main/java/nz/ac/auckland/se206/controllers/ChatController.java
+++ src/main/java/nz/ac/auckland/se206/controllers/ChatController.java
@@ -7,7 +7,6 @@ import javafx.scene.control.Button;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
 import nz.ac.auckland.se206.App;
-import nz.ac.auckland.se206.GameState;
 import nz.ac.auckland.se206.gpt.ChatMessage;
 import nz.ac.auckland.se206.gpt.GptPromptEngineering;
 import nz.ac.auckland.se206.gpt.openai.ApiProxyException;
@@ -84,7 +83,7 @@ public class ChatController {
     appendChatMessage(msg);
     ChatMessage lastMsg = runGpt(msg);
     if (lastMsg.getRole().equals("assistant") && lastMsg.getContent().startsWith("Correct")) {
-      GameState.isRiddleResolved = true;
+      // GameState.isRiddleResolved = true;
     }
   }
 
