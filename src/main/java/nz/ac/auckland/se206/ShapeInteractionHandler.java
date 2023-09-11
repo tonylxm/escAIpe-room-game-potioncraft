53849006d6d6996fa9ab0f1ff7d80ebc12ff41d2
Gluglug7 <abod278@aucklanduni.ac.nz>
2023-09-11 18:07:49 +1200

    ✨Keeing items in library room glowing after clicking them to know which oone you have currently selected

--- src/main/java/nz/ac/auckland/se206/ShapeInteractionHandler.java
+++ src/main/java/nz/ac/auckland/se206/ShapeInteractionHandler.java
@@ -23,15 +23,19 @@ public class ShapeInteractionHandler implements EventHandler<MouseEvent> {
     }
   }
 
+  /** Adding specified glow to images in a room. */
   public void glowThis(ImageView image) {
     image.setEffect(new Glow(1.2));
   }
 
+  /** Turning off the glow of an image unless it's been clicked. */
   public void unglowThis(ImageView image, boolean clicked) {
     if (!clicked) {
       image.setEffect(new Glow(0));
     }
   }
+
+  /** Turning off the glow of an image no matter what. */
   public void unglowThis(ImageView image) {
     image.setEffect(new Glow(0));
   }
