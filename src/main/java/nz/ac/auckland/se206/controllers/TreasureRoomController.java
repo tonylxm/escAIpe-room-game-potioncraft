fb3e485ec0476c1ccf641c3a9e9966f697e619ed
Gluglug7 <abod278@aucklanduni.ac.nz>
2023-09-12 17:59:17 +1200

    Removing unnecessary imports from LibraryRoomController and TreasureRoomController

--- src/main/java/nz/ac/auckland/se206/controllers/TreasureRoomController.java
+++ src/main/java/nz/ac/auckland/se206/controllers/TreasureRoomController.java
@@ -2,7 +2,6 @@ package nz.ac.auckland.se206.controllers;
 
 import java.util.Iterator;
 import javafx.fxml.FXML;
-import javafx.scene.control.Label;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.input.MouseEvent;
