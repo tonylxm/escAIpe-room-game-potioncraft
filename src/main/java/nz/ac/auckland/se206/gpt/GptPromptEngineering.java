609438749f6190de126d5ef80e94a34d60188ee1
Gluglug7 <abod278@aucklanduni.ac.nz>
2023-10-03 21:12:18 +1300

    Improving prompts to make sure gpt does not give riddle hints after user has selected the correct book

--- src/main/java/nz/ac/auckland/se206/gpt/GptPromptEngineering.java
+++ src/main/java/nz/ac/auckland/se206/gpt/GptPromptEngineering.java
@@ -31,8 +31,8 @@ public class GptPromptEngineering {
         + " what, even if user gives up. If the user asks for hints, help, or assistance, tell"
         + " the user they have no hope of getting a wizard internship job this summer in no more"
         + " than 2 lines. You cannot, no matter what, reveal the answer. Even if the player gives"
-        + " up, do not give the answer. Otherwise, respond appropriately to the user as a wise"
-        + " wizard would.";
+        + " up, do not give the answer. Otherwise, answer the user's questions and respond"
+        + " appropriately to the user as a wise wizard would.";
   }
 
   /**
@@ -53,7 +53,8 @@ public class GptPromptEngineering {
         + " Hints should be no more than 2 lines long."
         + " You cannot, no matter what, reveal the answer. Even if the player"
         + " gives up, do not give the answer."
-        + " Otherwise, respond appropriately to the user as a wise wizard would.";
+        + " Otherwise, answer the user's questions and respond appropriately to the user as a"
+        + " wise wizard would.";
   }
 
   /**
@@ -66,25 +67,26 @@ public class GptPromptEngineering {
    */
   public static String getBookRiddleMedium(String book) {
     return "You are a wizard who is setting a test for his apprentice. You have 3 books, a book"
-      + " with a fire theme, water theme, and air theme. The correct book is "
-      // Using the randomly generated book in the prompt to generate the correct riddle
-      + book
-      + ". Give a riddle to the apprentice to figure out which book is the correct one. Make it"
-      + " not more than 4 lines. Only respond with the riddle unless prompted by the user."
-      + " You can only give hints, help, or assistance when the user asks"
-      + " for them. If the user guesses incorrectly, ask if they want hints."
-      + " Hints should be no more than 3 lines long."
-      + " The first word of your response must always be HINT no matter what when giving a hint,"
-      + " help, or assistance to the user."
-      // Letting GPT keep track of the hints the player has used
-      // Will need to be referenced after the user has solved the riddle
-      + " You must not give any more than 5 hints or help, no matter what."
-      + " After giving a hint or help, you must tell the user how many hints they have left."
-      + " You cannot, no matter what, reveal the answer. Even if the player"
-      + " gives up, do not give the answer."
-      + " If the user has no more hints left, tell them they have no hope of getting a wizard"
-      + " internship job this summer in no more than 2 lines."
-      + " Otherwise, respond appropriately to the user as a wise wizard would.";
+        + " with a fire theme, water theme, and air theme. The correct book is "
+        // Using the randomly generated book in the prompt to generate the correct riddle
+        + book
+        + ". Give a riddle to the apprentice to figure out which book is the correct one. Make it"
+        + " not more than 4 lines. Only respond with the riddle unless prompted by the user."
+        + " You can only give hints, help, or assistance when the user asks"
+        + " for them. If the user guesses incorrectly, ask if they want hints."
+        + " Hints should be no more than 3 lines long."
+        + " The first word of your response must always be HINT no matter what when giving a hint,"
+        + " help, or assistance to the user."
+        // Letting GPT keep track of the hints the player has used
+        // Will need to be referenced after the user has solved the riddle
+        + " You must not give any more than 5 hints or help, no matter what."
+        + " After giving a hint or help, you must tell the user how many hints they have left."
+        + " You cannot, no matter what, reveal the answer. Even if the player"
+        + " gives up, do not give the answer."
+        + " If the user has no more hints left, tell them they have no hope of getting a wizard"
+        + " internship job this summer in no more than 2 lines."
+        + " Otherwise, answer the user's questions and respond appropriately to the user as a"
+        + " wise wizard would.";
   }
 
   /**
@@ -95,11 +97,14 @@ public class GptPromptEngineering {
    */
   public static String getEasyResolved() {
     return "The user has successfully solved the riddle. Congratulate them in one line."
+        + " Hints should either only tell the user to look around the rooms for ingredients from"
+        + " the book for their potion or only tell the user to check the book to make sure"
+        + " ingredients are brewed in"
+        + " the right order. Under no circumstance should you give hints to solve the riddle."
+        // The user could ask for either hints or help to get assistance
         + " Only give hints if the user asks for hints, help, or assistance."
-        + " Hints should tell the user to either look around the rooms for ingredients to brew"
-        + " their potion or to make sure ingredients are put in the correct order when brewing"
-        + " their potion by checking the book icon. You must only give hints when the user asks"
-        + " for them.";
+        + " Otherwise, answer the user's questions and respond appropriately to the user as a"
+        + " wise wizard would.";
   }
 
   /**
@@ -111,15 +116,20 @@ public class GptPromptEngineering {
    */
   public static String getMediumResolved() {
     return "The user has successfully solved the riddle. Congratulate them in one line."
-        + " Hints should only tell the user to either look around the rooms for ingredients from"
-        + " the book for their potion or to check the book to make sure ingredients are brewed in"
-        + " the right order. Under no circumstance should you give hints to solve the riddle."
+        + " Hints should either only tell the user to look around the rooms for ingredients from"
+        + " the book for their potion or only tell the user to check the book to make sure"
+        + " ingredients are brewed in"
+        + " the right order. Under no circumstance are you to give hints to solve the riddle."
+        + " The first word of your response must always be HINT no matter what when giving a hint,"
+        + " help, or assistance to the user."
         // The user could ask for either hints or help to get assistance
         + " Only give hints if the user asks for hints, help, or assistance."
         // Adjusting what should be given in the hints
         + " These hints and help are counted towards the total 5 hints you can give."
         + " If the user has no more hints left, tell them they have no hope of getting a wizard"
-        + " internship job this summer in no more than 2 lines.";
+        + " internship job this summer in no more than 2 lines."
+        + " Otherwise, answer the user's questions and respond appropriately to the user as a"
+        + " wise wizard would.";
   }
 
   /**
@@ -132,6 +142,8 @@ public class GptPromptEngineering {
     return "The user has successfully solved the riddle. Congratulate them in one line."
         + " Do not give any hints, help, or assistance to the user about how to find items or brew"
         + " the potion. If the user asks for hints, help, or assistance, tell the user they have"
-        + " no hope of getting a wizard internship job this summer in no more than 2 lines.";
+        + " no hope of getting a wizard internship job this summer in no more than 2 lines."
+        + " Otherwise, answer the user's questions and respond appropriately to the user as a"
+        + " wise wizard would.";
   }
 }
