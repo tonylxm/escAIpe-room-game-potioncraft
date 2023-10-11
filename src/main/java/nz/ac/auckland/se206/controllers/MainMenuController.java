bdd8b53a09ac6463f21fe64f661b3eb9fc4f3173
tonylxm <126369686+tonylxm@users.noreply.github.com>
2023-10-12 11:26:38 +1300

    ♻ Codestyle changes for main menu
    Changed access modifiers and naming conventions.

--- src/main/java/nz/ac/auckland/se206/controllers/MainMenuController.java
+++ src/main/java/nz/ac/auckland/se206/controllers/MainMenuController.java
@@ -526,7 +526,7 @@ public class MainMenuController {
    * the play button is clicked. Only called when the mouse enters the button.
    */
   @FXML
-  public void playGame() throws InterruptedException, IOException {
+  private void onPlayGame() throws InterruptedException, IOException {
     // Initialising the book and the chat handler whenever a new game is started from the
     // play button to have enough time to intialise the chat messages
     chatHandler = new ChatHandler();
@@ -652,7 +652,7 @@ public class MainMenuController {
    * continue button is clicked. Only called when the mouse enters the button.
    */
   @FXML
-  public void onContinueGame() {
+  private void onContinueGame() {
     continueBtn.setDisable(true);
     continueBtn.setOpacity(0.4);
     // Using a task to make sure game does not freeze
@@ -677,7 +677,7 @@ public class MainMenuController {
    * continue button is clicked. Only called when the mouse enters the button.
    */
   @FXML
-  public void onContinueGameOne() {
+  private void onContinueGameOne() {
     continueBtnOne.setDisable(true);
     continueBtnOne.setOpacity(0.4);
     // Using a task to make sure game does not freeze
@@ -1011,7 +1011,7 @@ public class MainMenuController {
    * @throws IOException the exception thrown when the file is not found.
    */
   @FXML
-  public void onStartGame() throws IOException, URISyntaxException {
+  private void onStartGame() throws IOException, URISyntaxException {
     System.out.println("MAIN MENU -> CAULDRON_ROOM");
     disableAndOrFadeTimeBtns(true, 0, false);
     // TransitionAnimation.changeScene(
