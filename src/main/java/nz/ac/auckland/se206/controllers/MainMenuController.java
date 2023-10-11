253fe86ae99490d621986221749392493e32860a
tonylxm <126369686+tonylxm@users.noreply.github.com>
2023-10-12 00:25:10 +1300

    ♻ Adjusted postiion of back button in cauldron.

--- src/main/java/nz/ac/auckland/se206/controllers/MainMenuController.java
+++ src/main/java/nz/ac/auckland/se206/controllers/MainMenuController.java
@@ -179,7 +179,6 @@ public class MainMenuController {
    * @return the countdown timer.
    */
   public static CountdownTimer getCountdownTimer() {
-    System.out.println("getting timer");
     return countdownTimer;
   }
 
